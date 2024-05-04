76b009173df23859e05094a907599c35e780e5d5
ThuRaPhyo <thuyaphyo7877@gmail.com>
2024-05-04 15:53:34 +0630

    solved

---
+++ script.js
@@ -0,0 +1,31 @@
+// TODO: What is `this` referring to here?
+console.log(this);
+// 'this' = browser or window. 
+
+// TODO: What is `this` referring to here?
+function helloThis() {
+  console.log('Inside this function, this is ' + this);
+  // 'this' mean a global variable 
+}
+
+// TODO: What is `this` referring to here? What will be logged in the console?
+var child = {
+  age: 10,
+  ageTenYears: function() {
+    console.log(this.age + 10);
+  }
+};
+
+// TODO: What is `this` referring to here? What will be logged in the console?
+var investor = {
+  name: 'Cash Saver',
+  investment: {
+    initialInvestment: 5000,
+    investmentGrowth: function() {
+      console.log(this.initialInvestment * 1.15);
+    }
+  }
+};
+
+// TODO: Call the `helloThis` function and the object methods to check results in the console
+helloThis();
